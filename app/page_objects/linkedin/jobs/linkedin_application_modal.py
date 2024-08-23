1282f87aeb50913965ec7b66c6788e5a9b9e6869
andrewkassab <andrewkassab@live.com>
2024-08-22 18:35:18 -0700

    made more progress on opening up job selections and iterating through the results

---
+++ app/page_objects/linkedin/jobs/linkedin_application_modal.py
@@ -0,0 +1,5 @@
+from jobbot.app.page_objects.base_page_object import BasePageObject
+
+
+class LinkedinApplicationModal(BasePageObject):
+    pass
--- app/page_objects/linkedin/jobs/linkedin_jobs_page.py
+++ app/page_objects/linkedin/jobs/linkedin_jobs_page.py
@@ -1,21 +1,24 @@
+from selenium.common import NoSuchElementException
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 
 from jobbot.app.enum.linkedin.linkedin_date_posted import LinkedinDatePosted
 from jobbot.app.enum.linkedin.linkedin_work_settings import LinkedinWorkSetting
 from jobbot.app.page_objects.linkedin.base_linkedin_page import BaseLinkedinPage
+from jobbot.app.page_objects.linkedin.jobs.linkedin_application_modal import LinkedinApplicationModal
 
 
 class LinkedinJobsPage(BaseLinkedinPage):
 
     __JOB_SEARCH_QUERY_FIELD = (By.XPATH, "//input[@id[contains(.,'jobs-search-box-keyword')]]")
     __JOB_SEARCH_LOCATION_FIELD = (By.XPATH, "//input[@id[contains(.,'jobs-search-box-location')]]")
-    __JOB_LIST_ITEM = (By.XPATH, "//li[contains(@class, 'jobs-search-results__list-item')]")
+    __JOB_LIST_ITEMS = (By.XPATH, "//li[contains(@class, 'jobs-search-results__list-item')]")
     __DATE_POSTED_FILTER_OPTIONS = (By.CSS_SELECTOR, "input[name='date-posted-filter-value']")
     __EASY_APPLY_BUTTON = (By.CSS_SELECTOR, "button[aria-label='Easy Apply filter.']")
     __WORK_SETTING_FILTER_OPTIONS = (By.CSS_SELECTOR, "input[name='remote-filter-value']")
     __ALL_FILTERS_BUTTON = (By.CSS_SELECTOR, "div[class='relative mr2']")
     __SHOW_RESULTS_BUTTON = (By.CSS_SELECTOR, "button[data-test-reusables-filters-modal-show-results-button='true']")
+    __APPLY_BUTTON = (By.XPATH, "//button[contains(@class,'jobs-apply-button')]")
 
     def __init__(self, driver):
         super().__init__(driver)
@@ -34,9 +37,12 @@ class LinkedinJobsPage(BaseLinkedinPage):
         search_query_field = self.find_element(self.__JOB_SEARCH_QUERY_FIELD)
         search_query_field.click()
         search_query_field.send_keys(Keys.ENTER)
+        return self
 
     def select_job_at_index(self, index):
-        pass
+        job_list_items = self.find_elements(self.__JOB_LIST_ITEMS)
+        job_list_items[index].click()
+        return self
 
     def set_filters(self, date_posted_filter: LinkedinDatePosted, work_setting_filters: [LinkedinWorkSetting]):
         all_filters_button = self.find_element(self.__ALL_FILTERS_BUTTON)
@@ -51,7 +57,25 @@ class LinkedinJobsPage(BaseLinkedinPage):
 
         show_results_button = self.find_element(self.__SHOW_RESULTS_BUTTON)
         show_results_button.click()
+        return self
 
     def select_easy_apply(self):
         easy_apply_button = self.find_element(self.__EASY_APPLY_BUTTON)
         easy_apply_button.click()
+        return self
+
+    def click_page(self, page_number):
+        try:
+            page_button = self.find_element((By.CSS_SELECTOR, f"button[aria-label='Page {page_number}']"))
+            page_button.click()
+            return self
+        except NoSuchElementException:
+            return False
+
+    def click_apply(self):
+        try:
+            apply_button = self.find_element(self.__APPLY_BUTTON)
+            apply_button.click()
+            return LinkedinApplicationModal()
+        except NoSuchElementException:
+            return False
