0f92a0b3af40a2c7e7b7810a477b67b06cef0ce6
andrewkassab <andrewkassab@live.com>
2024-08-28 17:57:05 -0700

    made it run headless

--- settings.py
+++ settings.py
@@ -6,6 +6,7 @@ import os
 from pathlib import Path
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 from jobbot.app.enum.dice.dice_posted_date import DicePostedDate
 from jobbot.app.enum.dice.dice_work_settings import DiceWorkSetting
@@ -13,12 +14,10 @@ from jobbot.app.util.parse_pdf import parse_pdf_to_text
 
 load_dotenv()
 
-logs_path = str((Path(__file__).parent / 'logs').resolve())
-resources_path = (Path(__file__).parent / 'app' / 'resources')
-RESUME_PATH = str((resources_path / 'resume.pdf').resolve())
-COVER_LETTER_PATH = str((resources_path / 'cover_letter.pdf').resolve())
 
 # Logging
+logs_path = str((Path(__file__).parent / 'logs').resolve())
+
 logging_formatter = logging.Formatter('%(asctime)s - %(message)s')
 
 success_logger = logging.getLogger('success_logger')
@@ -33,14 +32,28 @@ failure_handler = logging.FileHandler(logs_path + "/failed_applications.log")
 failure_handler.setFormatter(logging_formatter)
 failure_logger.addHandler(failure_handler)
 
+
+# Resume
+resources_path = (Path(__file__).parent / 'app' / 'resources')
+RESUME_PATH = str((resources_path / 'resume.pdf').resolve())
+COVER_LETTER_PATH = str((resources_path / 'cover_letter.pdf').resolve())
 try:
     RESUME_TEXT = parse_pdf_to_text(RESUME_PATH)
 except:
     logging.error('Error parsing resume, are you sure there is a resume.pdf in app/resources?')
 
-driver = webdriver.Chrome()
+
+# Driver
+chrome_options = Options()
+chrome_options.add_argument('--headless') # Run in headless mode
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--window-size=1920,1080')
+
+driver = webdriver.Chrome(options=chrome_options)
+
 driver.implicitly_wait(2)
 DRIVER_EXPLICIT_WAIT = 2
+
 
 # Activates Job description matching and cover letter writing but requires the
 # OPENAI_APY_KEY environment variable to be set.
@@ -48,11 +61,13 @@ USE_AI = True
 if os.environ.get('OPENAI_API_KEY') is None:
     USE_AI = False
 
-# Env Variables
+
+# Credentials
 DICE_EMAIL = os.environ.get('DICE_EMAIL')
 DICE_PASSWORD = os.environ.get('DICE_PASSWORD')
 LINKEDIN_EMAIL = os.environ.get('LINKED_EMAIL')
 LINKEDIN_PASSWORD = os.environ.get('LINKEDIN_PASSWORD')
+
 
 # DICE job search configuration
 POSTED_DATE = DicePostedDate.LAST_7_DAYS
